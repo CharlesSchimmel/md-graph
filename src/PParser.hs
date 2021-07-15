b9f64fce29f31b1940ab5d4ee892fdf4cc272a7d
CharlesSchimmel <charlesschimmel@gmail.com>
2021-07-15 17:37:57 -0500

    refine imports

--- src/PParser.hs
+++ src/PParser.hs
@@ -2,12 +2,14 @@ module PParser where
 
 import           Data.HashSet                  as S
 import           Data.Text                     as T
-import           Text.Pandoc.Class
-import           Text.Pandoc.Definition
-import           Text.Pandoc.Error
+import           Text.Pandoc.Class              ( runPure )
+import           Text.Pandoc.Definition         ( Inline(..) )
+import           Text.Pandoc.Error              ( PandocError )
 import           Text.Pandoc.Options
-import           Text.Pandoc.Readers
-import           Text.Pandoc.Walk
+import           Text.Pandoc.Readers            ( readMarkdown
+                                                , readVimwiki
+                                                )
+import           Text.Pandoc.Walk               ( query )
 
 sieveLinks :: Text -> Maybe [Text]
 sieveLinks content = either (const Nothing) (Just . S.toList) allLinks
