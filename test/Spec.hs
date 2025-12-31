f234907d8a2b2b58dfc381e00af53f6af8b7f394
Charles Schimmelpfennig <charlesschimmel@gmail.com>
2025-12-31 11:52:39 -0600

    Update tests for new scanning options

--- test/Spec.hs
+++ test/Spec.hs
@@ -33,10 +33,11 @@ import Test.Hspec
 main :: IO ()
 main = do
   libraryDir <- getLibraryDir
-  hspec FilesSpec.spec
-  hspec $ SubgraphSpec.spec libraryDir
-  -- hspec $ populateSpec
   hspec $ do
+    FilesSpec.spec
+    SubgraphSpec.spec libraryDir
+    populateSpec
+
     let baseSgOptions =
           SubgraphOptions
             { sgInclNonex = False,
@@ -132,31 +133,31 @@ main = do
         let args = defaultSpecArgs {argCommand = command}
         mdGraph args >>= outputContains [Constants.parent_md]
 
--- populateSpec :: Spec
--- populateSpec = do
---   describe "Populate" $ do
---     it "User can specify specific files to populate and parse" $ do
---       libraryDir <- getLibraryDir
---       withTempDbFile $ \args -> do
---         let dbPath = dbFile $ argDatabase args
+populateSpec :: Spec
+populateSpec = do
+  describe "Populate" $ do
+    it "User can specify specific files to populate and parse" $ do
+      libraryDir <- getLibraryDir
+      withTempDbFile $ \args -> do
+        let dbPath = dbFile $ argDatabase args
 
---         -- Populate only linkChain2_md
---         let scanOpt = ScanSome [libraryDir </> Constants.linkChain2_md]
---         let args' = args {argCommand = Populate}
---         mdGraph args'
+        -- Populate only linkChain2_md
+        let scanOpt = ScanSome [libraryDir </> Constants.linkChain2_md]
+        let args' = args {argCommand = Populate, argScan = scanOpt}
+        mdGraph args'
 
---         -- It should be returned as unreachable (even though linkChain1_md links to it).
---         rawQueryResults <- runSqlite dbPath $ getAllDocuments
---         let dbDocuments = fmap documentPath $ entityVal <$> rawQueryResults
---         dbDocuments `shouldContain` [Constants.linkChain2_md]
---         dbDocuments `shouldNotContain` [Constants.linkChain1_md]
+        -- It should be returned as unreachable (even though linkChain1_md links to it).
+        rawQueryResults <- runSqlite dbPath $ getAllDocuments
+        let dbDocuments = fmap documentPath $ entityVal <$> rawQueryResults
+        dbDocuments `shouldContain` [Constants.linkChain2_md]
+        dbDocuments `shouldNotContain` [Constants.linkChain1_md]
 
---         -- Populate linkChain1
---         let scanOpt = ScanSome [libraryDir </> Constants.linkChain1_md]
---         let args' = args {argCommand = Populate, argScan}
---         _ <- mdGraph args'
+        -- Populate linkChain1
+        let scanOpt = ScanSome [libraryDir </> Constants.linkChain1_md]
+        let args' = args {argCommand = Populate, argScan = scanOpt}
+        _ <- mdGraph args'
 
---         -- Get the forwardLinks of linkChain1_md, it should contain linkChain2_md
---         rawQueryResults <- runSqlite dbPath $ forwardLinks Constants.linkChain1_md
---         let queryResultPaths = fmap documentPath $ entityVal <$> rights rawQueryResults
---         queryResultPaths `shouldContain` [Constants.linkChain2_md]
+        -- Get the forwardLinks of linkChain1_md, it should contain linkChain2_md
+        rawQueryResults <- runSqlite dbPath $ forwardLinks Constants.linkChain1_md
+        let queryResultPaths = fmap documentPath $ entityVal <$> rights rawQueryResults
+        queryResultPaths `shouldContain` [Constants.linkChain2_md]
